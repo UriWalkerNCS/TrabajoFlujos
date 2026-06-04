42eb63054651c46afc984857e97b0c0fbc2bcd29
UriWalkerNCS <sanchezurivic05@gmail.com>
2026-06-04 08:51:45 -0600

    Modulo de implementacion con explicacion funcionales

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,3 +1,5 @@
+//Se crearon las propiedades de la clase CreateCatDTO que se utilizara para crear un nuevo gato en la aplicacion.
+//Es de suma importante que se creen correctamente las propiedades de la clase CreateCatDTO
 export class CreateCatDto {
   nombre!: string;
   edad!: number;
