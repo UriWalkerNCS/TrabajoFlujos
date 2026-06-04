7102d5e8cb735c6a97195aadd11ac82f0c4cacc1
UriWalkerNCS <sanchezurivic05@gmail.com>
2026-06-04 15:24:31 -0600

    Actualización del archivo service

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -3,7 +3,11 @@ import { Cat } from './types/cat.entity';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
 
+// Decorador que marca esta clase como un proveedor que puede ser inyectado en otros componentes (como controladores).
+// Los servicios contienen la lógica de negocio y son consumidos por controladores
 @Injectable()
+ // Base de datos en memoria (arreglo de gatos)
+  // Datos de ejemplo iniciales para probar la funcionalidad del servicio
 export class CatsService {
   private cats: Cat[] = [
     {
@@ -26,41 +30,44 @@ export class CatsService {
     },
   ];
 
+   // Este metodo obtiene todos los gatos
   findAll() {
     return this.cats;
   }
-
+  // Busca un gato por su ID
   findOne(id: number) {
     return this.cats.find((cat) => cat.id === id);
   }
-
+ // Crea un nuevo gato
   create(createCatDto: CreateCatDto) {
     const newCat: Cat = {
       id: this.cats.length + 1,
       ...createCatDto,
     };
-
+//Se agrega el nuevo gato al arreglo de gatos
     this.cats.push(newCat);
     return newCat;
   }
-
+//Actualiza un gato existente por su ID después de encontrarlo. Si el gato existe, se actualizan 
+//sus propiedades con los datos proporcionados en updateCatDto.
   update(id: number, updateCatDto: UpdateCatDto) {
     const cat = this.findOne(id);
 
     if (cat) {
       Object.assign(cat, updateCatDto);
     }
-
+// Se devuelve el gato actualizado (o undefined si no se encontró el gato)
     return cat;
   }
-
+//Se elimina un gato por su ID despues de encontrarlo. Si el gato existe
+//Se elimina del arreglo de gatos y se devuelve un mensaje de confirmación.
   remove(id: number) {
     const index = this.cats.findIndex((cat) => cat.id === id);
 
     if (index !== -1) {
       this.cats.splice(index, 1);
     }
-
+ 
     return {
       mensaje: 'Gato eliminado correctamente',
     };
