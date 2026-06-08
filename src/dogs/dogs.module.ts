5b65c5babf9838ea3348174928cc39321754727e
UriWalkerNCS <sanchezurivic05@gmail.com>
2026-06-08 14:04:39 -0600

    Se implemento correctamente el modulo de dogs

--- src/dogs/dogs.module.ts
+++ src/dogs/dogs.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DogsController } from './dogs.controller';
 import { DogsService } from './dogs.service';
-
+//El módulo DogsModule agrupa el controlador DogsController y
+//El servicio DogsService relacionados con la entidad Dog. 
+//En NestJS, un módulo es una forma de organizar el código en unidades cohesivas, facilitando la gestión de dependencias y la modularización de la aplicación.
 @Module({
   controllers: [DogsController],
   providers: [DogsService]
